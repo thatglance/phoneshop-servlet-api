a5a0f6cf9edeb1ad38ef4f747e220272f45d6fba
thatglance <11sashamak@gmail.com>
2018-11-11 13:15:51 +0300

    implement ArrayListProductDao, add dao in ProductListPageServlet, implement tests

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,41 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+    private List<Product> productsList = new ArrayList<>();
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized Product getProduct(Long id) {
+        for (Product product : productsList) {
+            if (product.getId().equals(id)) {
+                return product;
+            }
+        }
+        return null;
     }
 
     @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+    public synchronized List<Product> findProducts() {
+        return productsList.stream().filter(p -> (p.getPrice() != null) && (p.getStock() > 0)).
+                collect(Collectors.toList());
     }
 
     @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void save(Product product) {
+        if (getProduct(product.getId()) == null) {
+            productsList.add(product);
+        }
     }
 
     @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void delete(Long id) {
+        for (int i = 0; i < productsList.size(); i++) {
+            if (productsList.get(i).getId().equals(id)) {
+                productsList.remove(i);
+            }
+        }
     }
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -3,6 +3,13 @@ package com.es.phoneshop.model.product;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class ArrayListProductDaoTest
@@ -17,5 +24,52 @@ public class ArrayListProductDaoTest
     @Test
     public void testFindProductsNoResults() {
         assertTrue(productDao.findProducts().isEmpty());
+    }
+
+    @Test
+    public void testGetProduct() {
+        Product product = new Product(2L, "sgs2", "Samsung Galaxy S II", new BigDecimal(200),
+                Currency.getInstance("USD"), 0, "https://raw.githubusercontent.com/andrewosipenko/" +
+                "phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg");
+        productDao.save(product);
+        assertEquals(product, productDao.getProduct(2L));
+        assertNull(productDao.getProduct(1L));
+    }
+
+    @Test
+    public void testFindProducts() {
+        Currency usd = Currency.getInstance("USD");
+        Product product1 = new Product(1L, "sgs", "Samsung Galaxy S II", new BigDecimal(100),
+                usd, 100, "https://raw.githubusercontent.com/andrewosipenko/" +
+                "phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        Product product2 = new Product(2L, "sgs2", "Samsung Galaxy S II", new BigDecimal(200),
+                usd, 0, "https://raw.githubusercontent.com/andrewosipenko/" +
+                "phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg");
+        Product product3 = new Product(3L, "sgs3", "Samsung Galaxy S III", null,
+                usd, 5, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/" +
+                "manufacturer/Samsung/Samsung%20Galaxy%20S%20III.jpg");
+        productDao.save(product1);
+        productDao.save(product2);
+        productDao.save(product3);
+        List<Product> expected = new ArrayList<>();
+        expected.add(product1);
+        List<Product> actual = productDao.findProducts();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDelete(){
+        Currency usd = Currency.getInstance("USD");
+        Product product1 = new Product(1L, "sgs", "Samsung Galaxy S II", new BigDecimal(100),
+                usd, 100, "https://raw.githubusercontent.com/andrewosipenko/" +
+                "phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg");
+        Product product2 = new Product(2L, "sgs2", "Samsung Galaxy S II", new BigDecimal(200),
+                usd, 0, "https://raw.githubusercontent.com/andrewosipenko/" +
+                "phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg");
+        productDao.save(product1);
+        productDao.save(product2);
+        assertEquals(product1, productDao.getProduct(1L));
+        productDao.delete(1L);
+        assertNull(productDao.getProduct(1L));
     }
 }
